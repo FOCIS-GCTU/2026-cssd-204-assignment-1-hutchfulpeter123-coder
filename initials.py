aa50311b96c7ef7ca30ad565f5edfdf8d071cc24
hutchfulpeter123-coder <hutchfulpeter123@gmail.com>
2026-05-31 20:05:53 +0000

    Add files via upload
    Peter Sagoe Acquaye - Hutchful (2425403164)

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Peter Acquaye Hutchful
+# STUDENT ID: 2425403164
+# Email: 2425403164@live.gctu.edu.gh
+# Grader: Augustus 
+#
+# On my honor, Peter Acquaye Hutchful, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # This function prints my initials in small and large format
+
+    print()
+    print("...PAH")
+    print()
 
 
+    print("...PPPPPPPPPPPP...AAAAAAAAAAAA...HHHHHHHHHHHH...****")
+    print("...PPPPPPPPPPPP...AAAAAAAAAAAA...HHHHHHHHHHHH...****")
+    print("...PP........PP...AA........AA...HH........HH...****")
+    print("...PP........PP...AA........AA...HH........HH...****")
+    print("...PPPPPPPPPPPP...AAAAAAAAAAAA...HHHHHHHHHHHH...****")
+    print("...PPPPPPPPPPPP...AAAAAAAAAAAA...HHHHHHHHHHHH...****")
+    print("...PP..............AA........AA...HH........HH...****")
+    print("...PP..............AA........AA...HH........HH...****")
+    print("...PP........PP...AA........AA...HH........HH...****")
+    print("...PP........PP...AA........AA...HH........HH...****")
+    print()
 
 main()
+
+    
